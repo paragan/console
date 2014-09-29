db614ccebc2c176648377830218cf1f85a711007
bodempudi <bodempudi@users.noreply.github.com>
2014-09-29 16:02:29 +0530

    few codes

--- outparametertest/trunk/outparametertest/Program.cs
+++ outparametertest/trunk/outparametertest/Program.cs
@@ -12,12 +12,8 @@ namespace outparametertest
     {
         static void Main(string[] args)
         {
-            //https://esro.eno.org/Customfeeds/performance.aspx?@FromEventDate=YYYY-MM-DD&@ToEventDate=YYYY-MM-DD
-             DateTime curDate = new DateTime(2014,9,20);
-             Console.WriteLine("the datetime is---"+curDate);
-             Console.WriteLine(curDate.AddDays(-1).ToString("yyyy-MM-dd"));
-             Console.WriteLine(curDate.AddDays(-2).ToString("yyyy-MM-dd"));
-             Console.Read();
+            
+            
         }
     }
 }
