73811e43fec677903d8dedb1559a1eca8cb9ef9d
bodempudi <bodempudi@users.noreply.github.com>
2014-09-29 15:58:58 +0530

    sample

---
+++ outparametertest/trunk/outparametertest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace outparametertest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://esro.eno.org/Customfeeds/performance.aspx?@FromEventDate=YYYY-MM-DD&@ToEventDate=YYYY-MM-DD
+             DateTime curDate = new DateTime(2014,9,20);
+             Console.WriteLine("the datetime is---"+curDate);
+             Console.WriteLine(curDate.AddDays(-1).ToString("yyyy-MM-dd"));
+             Console.WriteLine(curDate.AddDays(-2).ToString("yyyy-MM-dd"));
+             Console.Read();
+        }
+    }
+}
